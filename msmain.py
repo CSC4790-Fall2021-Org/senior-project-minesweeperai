bfe19baa4d7b2e92b9309ca393e8e5bdf7dde6c5
David Joyce <55989694+djoyce6@users.noreply.github.com>
2021-09-10 21:26:07 -0400

    added comments

--- msmain.py
+++ msmain.py
@@ -5,6 +5,9 @@ from directKeys import click, queryMousePosition, PressKey, ReleaseKey, SPACE
 import time
 import math
 
+# My screen coordinates for the Minesweeper board
 board_coords = [37, 157, 1343, 983]
 
-screen = np.array(ImageGrab.grab(bbox=board_coords))
+while True:
+    break
+    # screen = np.array(ImageGrab.grab(bbox=board_coords))
