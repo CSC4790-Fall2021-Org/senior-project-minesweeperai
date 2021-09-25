26a0178de05a9f9a07009fa834a3fb1194132e8f
David Joyce <djoyce6@villanova.edu>
2021-09-24 22:48:51 -0400

    updates prior to first sprint review
    Updated RGB values for the numbers, added a for loop to loop through the whole grid and identify numbers by RGB value. Updated default pixel increment to 42 (approximate number of pixels across a square of MineSweeper)

--- msmain.py
+++ msmain.py
@@ -1,20 +1,22 @@
 import numpy as np
+import PIL
 from PIL import ImageGrab
-import cv2
+#import cv2
 from directKeys import click, queryMousePosition, PressKey, ReleaseKey, SPACE
 import time
 import math
 
 # My screen coordinates for the Minesweeper board
 board_coords = [37, 157, 1343, 983]
+pixel_increment = 42
 
 # RGB values for the differnt colors
-#[189, 189, 189] # rgb value for 0
-#[0, 33, 245] # rgb value for 1
-#[53, 120, 32] # rgb value for 2
-#[234, 50, 35] # rgb value for 3
-#[5, 0, 123] # rgb value for 4
-#[123, 1, 0] # rgb value for 5
+#[128, 128, 128] # rgb value for 0
+rgbOne = (0, 0, 170) # rgb value for 1
+rgbTwo = (75, 110, 75) # rgb value for 2
+rgbThree = (170, 0, 0) # rgb value for 3
+rgbFour = (0, 0, 85) # rgb value for 4
+rgbFive = (85, 0, 0) # rgb value for 5
 
 # Begin the program if mouse position is on the left of the screen
 while True:
@@ -22,13 +24,30 @@ while True:
     if mouse_pos.x <= 0:
         break
 
-def click_middle(screen):
-    click(board_coords[2]/2,board_coords[3]/2)
+#def click_middle(screen):
+    #click(board_coords[2]/2,board_coords[3]/2)
+    
+num1s = 0
+num2s = 0
+num3s = 0
+num4s = 0
+num5s = 0
 
 print("clicker is ready to go")
 while True:
+    first_click = [668,603]
+    mouse_pos = queryMousePosition()
     screen = np.array(ImageGrab.grab(bbox=board_coords))
-    click_middle(screen)
+    click(first_click[0],first_click[1])
+    for x in range (first_click[0]-(pixel_increment*8),first_click[0]+(pixel_increment*8),pixel_increment):
+        for y in range (first_click[1]-(pixel_increment*8),first_click[1]+(pixel_increment*8),pixel_increment):
+            pixel_rgb = PIL.ImageGrab.grab().load()[x,y]
+            
+            if pixel_rgb == rgbTwo:
+                click(x,y)
+                time.sleep(.05)
+                
+    print("clicker is finished")
     break
     
 
