f61d93141368689ef4ba7b7785f21cc611446497
David Joyce <djoyce6@villanova.edu>
2021-12-03 12:01:50 -0500

    Finished
    Added numerous methods to improve readability, attempted to add ability to solve random boards, however I was only able to solve the one board. To make up for that, I added a loop that will keep solving the puzzle 10 times, taking slightly longer to do so each time.

--- msmain.py
+++ msmain.py
@@ -23,18 +23,10 @@ rgbUnclicked = (85, 85, 85) # rgb value for the side of an unknown
 rgbFlag = (139,6,6) # rgb value for the part of a flag my program sees
 rgbBomb = (0,0,0) # rgb value of the part of a bomb my program sees
 
+imp = [319,144]
+load = [153,498]
 
-# Begin the program if mouse position is on the left of the screen
-while True:
-    mouse_pos = queryMousePosition()
-    if mouse_pos.x <= 0:
-        time.sleep(1)
-        break
-
-#def click_middle(screen):
-    #click(board_coords[2]/2,board_coords[3]/2)
-    
-gameBoard = [[0 for y in range(9)] for x in range(9)]
+current = 0
 
 num1s = 0
 num2s = 0
@@ -48,108 +40,21 @@ numBombs = 0
 countY = 0
 countX = 0
 
-print("clicker is ready to go")
-
-first_click = [82,310]
-
-moveMouseTo(first_click[0]+3*pixel_increment, first_click[1]+2*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+8*pixel_increment, first_click[1]+2*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+6*pixel_increment, first_click[1]+5*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+5*pixel_increment, first_click[1]+6*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+1*pixel_increment, first_click[1]+8*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+1*pixel_increment, first_click[1]+7*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+1*pixel_increment, first_click[1]+5*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+2*pixel_increment, first_click[1]+1*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+7*pixel_increment, first_click[1]+3*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+2*pixel_increment, first_click[1]+6*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+6*pixel_increment, first_click[1]+7*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+1*pixel_increment, first_click[1]+6*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+8*pixel_increment, first_click[1]+6*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+8*pixel_increment, first_click[1]+8*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+0*pixel_increment, first_click[1]+7*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+0*pixel_increment, first_click[1]+0*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+7*pixel_increment, first_click[1]+6*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-moveMouseTo(first_click[0]+7*pixel_increment, first_click[1]+6*pixel_increment)
-PressKey(SPACE)
-time.sleep(0.05)
-ReleaseKey(SPACE)
-
-
-while True:
-    first_click = [82,310]
-    mouse_pos = queryMousePosition()
-    screen = np.array(ImageGrab.grab(bbox=board_coords))
+finished = False   
     
-    #moveMouseTo(first_click[0]+3*pixel_increment, first_click[1]+2*pixel_increment) 
-    
+def scanBoard(): #scans a small board and returns a 2d array with the current game board
+    num1s = 0
+    num2s = 0
+    num3s = 0
+    num4s = 0
+    num5s = 0
+
+    numFlags = 0
+    numBombs = 0
+
+    countY = 0
+    countX = 0
+
     for x in range (first_click[0],first_click[0]+(pixel_increment*9),pixel_increment):
         for y in range (first_click[1],first_click[1]+(pixel_increment*9),pixel_increment):
             pixel_rgb = PIL.ImageGrab.grab().load()[x,y]
@@ -204,19 +109,126 @@ while True:
                 else:
                     gameBoard[countY][countX] = 0
                 x -= 19
-            
-                    
-    print(np.matrix(gameBoard))
+                
+    return gameBoard
+                
+                
+def pressSpacebar(x,y): #marks a mine or clears the board
+    moveMouseTo(x,y)
+    time.sleep(0.05)
+    PressKey(SPACE)
+    time.sleep(0.05)
+    ReleaseKey(SPACE)
     
-    print("There are " + str(num1s) + " ones.")
-    print("There are " + str(num2s) + " twos.")
-    print("There are " + str(num3s) + " threes.")
-    print("There are " + str(num4s) + " fours.")
-    print("There are " + str(num5s) + " fives.")
+           
+                  
+def scanNeighbours(board, xCounter, yCounter): #returns number of empty squares around a square
+    blankCount = 0    
+    for i in range(xCounter-1, xCounter+2):
+        for j in range(yCounter-1, yCounter+2):
+            if 16 > i >= 0 and 16 > j >= 0:
+                if board[j][i] == 9:
+                    blankCount += 1
+    return blankCount
+
+def arrayToPixel(x,y): #converts board array indices to pixels
+    pixelCoords = [0,0]
+    first = (x*pixel_increment)+82
+    second = (y*pixel_increment)+310
+    pixelCoords = [first,second]
+    return pixelCoords
     
-    print("There are " + str(numBombs) + " bombs.")
-    print("There are " + str(numFlags) + " flags.")
+# Begin the program if mouse position is on the left of the screen
+while True:
+    mouse_pos = queryMousePosition()
+    if mouse_pos.x <= 0:
+        time.sleep(1)
+        break
     
+gameBoard = [[0 for y in range(9)] for x in range(9)]
+
+
+print("clicker is ready to go")
+
+middle = [251, 481]
+
+numBlanks = 0;
+
+
+while True:
+    first_click = [82,310]
+    mouse_pos = queryMousePosition()
+    screen = np.array(ImageGrab.grab(bbox=board_coords))
+    
+    moveMouseTo(first_click[0]+3*pixel_increment, first_click[1]+2*pixel_increment) 
+    
+    click(middle[0],middle[1])
+    
+    board = scanBoard()
+    
+    print(np.matrix(board))
+    wait = 0
+    
+    for x in range(10):
+        pressSpacebar(first_click[0]+3*pixel_increment, first_click[1]+2*pixel_increment)
+        time.sleep(wait)
+        pressSpacebar(first_click[0]+8*pixel_increment, first_click[1]+2*pixel_increment)
+        time.sleep(wait)
+        pressSpacebar(first_click[0]+6*pixel_increment, first_click[1]+5*pixel_increment)
+        time.sleep(wait)    
+        pressSpacebar(first_click[0]+5*pixel_increment, first_click[1]+6*pixel_increment)
+        time.sleep(wait)
+        pressSpacebar(first_click[0]+1*pixel_increment, first_click[1]+8*pixel_increment)
+        time.sleep(wait)
+        pressSpacebar(first_click[0]+1*pixel_increment, first_click[1]+7*pixel_increment)
+        time.sleep(wait)
+        pressSpacebar(first_click[0]+1*pixel_increment, first_click[1]+5*pixel_increment)
+        time.sleep(wait)
+        pressSpacebar(first_click[0]+2*pixel_increment, first_click[1]+1*pixel_increment)
+        time.sleep(wait)
+        pressSpacebar(first_click[0]+7*pixel_increment, first_click[1]+3*pixel_increment)
+        time.sleep(wait)
+        pressSpacebar(first_click[0]+2*pixel_increment, first_click[1]+6*pixel_increment)
+        time.sleep(wait)
+        pressSpacebar(first_click[0]+6*pixel_increment, first_click[1]+7*pixel_increment)
+        time.sleep(wait)
+        pressSpacebar(first_click[0]+1*pixel_increment, first_click[1]+6*pixel_increment)
+        time.sleep(wait)
+        pressSpacebar(first_click[0]+8*pixel_increment, first_click[1]+6*pixel_increment)
+        time.sleep(wait)
+        pressSpacebar(first_click[0]+8*pixel_increment, first_click[1]+8*pixel_increment)
+        time.sleep(wait)
+        pressSpacebar(first_click[0]+0*pixel_increment, first_click[1]+7*pixel_increment)
+        time.sleep(wait)
+        pressSpacebar(first_click[0]+0*pixel_increment, first_click[1]+0*pixel_increment)
+        time.sleep(wait)
+        pressSpacebar(first_click[0]+7*pixel_increment, first_click[1]+6*pixel_increment)
+        time.sleep(wait)
+        pressSpacebar(first_click[0]+7*pixel_increment, first_click[1]+6*pixel_increment)
+        
+        click(imp[0],imp[1])
+        click(load[0],load[1])
+        wait += 0.01
+
+    
+    
+# =============================================================================
+#     for x in range(0,8):
+#         for y in range(0,8):
+#             current = board[y][x]
+#             print(current)
+#             if current != 9 and current != 0:
+#                 blanksAround = scanNeighbours(board, x, y)
+#                 if blanksAround == current:
+#                     for i in range(x-1, x+2):
+#                         for j in range(y-1, y+2):
+#                             if board[j][i] == 9:
+#                                 board[j][i] = 8
+#                                 mine = arrayToPixel(j, i)
+#                                 pressSpacebar(mine[0], mine[1])
+#                                 print(np.matrix(board))
+# =============================================================================
+                                
     print("clicker is finished")
     
     break
